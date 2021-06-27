0def4147ae0f6c54da117b1cb826d91f72174905
unknown <abhinandanraul@gmail.com>
2021-06-27 23:53:58 +0530

    added database integration

--- mood.py
+++ mood.py
@@ -9,6 +9,7 @@ password='1234'
 # --------------------------------------
 
 st.title('Mood Match')
+st.subheader('Need someone to talk to! MoodMatch finds a person with a similar mood to you.')
 add_selectbox = st.sidebar.selectbox(
     "NAVIGATION",  # Title of sidebar
     (
@@ -92,69 +93,85 @@ if add_selectbox== 'Login':
             moodindex_list.append(x[7])
         mydb.commit()
 
-    email=st.text_input('Enter email')
-    user_password=st.text_input('Enter password')
-    btn=True
-    if(password_verify(email,user_password) and btn):
-        username=Find_Name(email)
-        st.subheader('Hi {}!'.format(username))
+    email=st.text_input('Enter your email')
+    user_password = st.text_input('Enter password',type='password')
 
-        st.header('Select your emotional levels')
-        """
-        Use the slider to increase and decrease levels for Happiness, Sadness, Fear, Disgust, Anger, Surprise
-        """
-        e1 = st.slider("Happiness",min_value=0.0, max_value=10.0,step=0.5,help='Select happiness level')
-        e2 = st.slider("Sadness",min_value=0.0, max_value=10.0, step=0.5)
-        e3 = st.slider("Fear",min_value=0.0, max_value=10.0, step=0.5)
-        e4 = st.slider("Disgust",min_value=0.0, max_value=10.0, step=0.5)
-        e5 = st.slider("Anger",min_value=0.0, max_value=10.0, step=0.5)
-        e6 = st.slider("Surprise",min_value=0.0, max_value=10.0, step=0.5)
+    def button():
+        k=st.button('Login')
+        return k
 
-        weight_dict={'Happiness':7,'Sadness':-4,
-                     'Fear':-2,'Disgust':-5,
-                     'Anger':-7,'Surprise':8} # Mentioning Weights for each emotion
+    def Login(email, user_password):
+        if password_verify(email, user_password):
+            return True
+        else:
+            return False
 
-        emotion_code_dict={'Happiness':e1,'Sadness':e2,
-                     'Fear':e3,'Disgust':e4,
-                     'Anger':e5,'Surprise':e6} # Access code for each emotion
+    def Main_Ops():
 
-        val_sum=0
-        for key in weight_dict:
-            val_sum+=weight_dict.get(key)*emotion_code_dict.get(key) # Sum of product of emotion levels and their weights
-        mood_index=round((val_sum/6.0),2)
-        st.subheader('Your Mood Index: {}'.format(mood_index))
+            username=Find_Name(email)
+            st.subheader('Hi {}!'.format(username))
 
-
-        if st.button('Submit'): # On submit do the following
-            if Is_Entry_Present(email):
-                Update_Entry(email, e1, e2, e3, e4, e5, e6, mood_index)
-            else:
-                Insert_Entry(email, e1, e2, e3, e4, e5, e6, mood_index)
-            st.success('Submitted')
-
-            my_mood_index = moodindex_list.index(mood_index)  # Index of my mood in sorted list
-            if (my_mood_index != len(moodindex_list) - 1):
-                mymatch_mood_index = moodindex_list[my_mood_index + 1];  # Extract a mood index succeeding my mood
-                mymatch_index = my_mood_index + 1  # Find the next mood index
-            else:
-                mymatch_mood_index = moodindex_list[my_mood_index - 1];  # Extract a mood index succeeding my mood
-                mymatch_index = my_mood_index - 1  # Find the next mood index
-
-            mymatch_email = email_list[mymatch_index]  # find email of the next/prev mood index
-            # st.write(mymatch_mood_index, mymatch_email) # To write matched persons email and mood index
-            st.balloons()
+            st.header('Select your emotional levels')
             """
-            ## You've got a match
+            Use the slider to increase and decrease levels for Happiness, Sadness, Fear, Disgust, Anger, Surprise
             """
-            st.subheader(Find_Name(mymatch_email))
-            st.write('Contact at:',mymatch_email)
+            e1 = st.slider("Happiness",min_value=0.0, max_value=10.0,step=0.5,help='Select happiness level')
+            e2 = st.slider("Sadness",min_value=0.0, max_value=10.0, step=0.5)
+            e3 = st.slider("Fear",min_value=0.0, max_value=10.0, step=0.5)
+            e4 = st.slider("Disgust",min_value=0.0, max_value=10.0, step=0.5)
+            e5 = st.slider("Anger",min_value=0.0, max_value=10.0, step=0.5)
+            e6 = st.slider("Surprise",min_value=0.0, max_value=10.0, step=0.5)
+
+            weight_dict={'Happiness':7,'Sadness':-4,
+                         'Fear':-2,'Disgust':-5,
+                         'Anger':-7,'Surprise':8} # Mentioning Weights for each emotion
+
+            emotion_code_dict={'Happiness':e1,'Sadness':e2,
+                         'Fear':e3,'Disgust':e4,
+                         'Anger':e5,'Surprise':e6} # Access code for each emotion
+
+            val_sum=0
+            for key in weight_dict:
+                val_sum+=weight_dict.get(key)*emotion_code_dict.get(key) # Sum of product of emotion levels and their weights
+            mood_index=round((val_sum/6.0),2)
+            st.subheader('Your Mood Index: {}'.format(mood_index))
+
+
+            if st.button('Submit'): # On submit do the following
+                if Is_Entry_Present(email):
+                    Update_Entry(email, e1, e2, e3, e4, e5, e6, mood_index)
+                else:
+                    Insert_Entry(email, e1, e2, e3, e4, e5, e6, mood_index)
+                st.success('Submitted')
+
+                my_mood_index = moodindex_list.index(mood_index)  # Index of my mood in sorted list
+                if (my_mood_index != len(moodindex_list) - 1):
+                    mymatch_mood_index = moodindex_list[my_mood_index + 1];  # Extract a mood index succeeding my mood
+                    mymatch_index = my_mood_index + 1  # Find the next mood index
+                else:
+                    mymatch_mood_index = moodindex_list[my_mood_index - 1];  # Extract a mood index succeeding my mood
+                    mymatch_index = my_mood_index - 1  # Find the next mood index
+
+                mymatch_email = email_list[mymatch_index]  # find email of the next/prev mood index
+                # st.write(mymatch_mood_index, mymatch_email) # To write matched persons email and mood index
+                st.balloons()
+                """
+                ## You've got a match
+                """
+                st.subheader(Find_Name(mymatch_email))
+                st.write('Contact at:',mymatch_email)
+    try:
+        if(Login(email, user_password)): # IF login is successful, run the main ops function
+            Main_Ops()
+    except:
+        st.warning('Enter email and password')
 
 if add_selectbox=='SignUp':
-    st.header('TheMoodMatch Login')
+    st.header('Create an account')
     name = st.text_input('Enter your name')
     email = st.text_input('Enter your email')
-    user_password = st.text_input('Enter your password')
-    password_conf = st.text_input('Enter password again')
+    user_password = st.text_input('Enter your password', type='password')
+    password_conf = st.text_input('Enter password again',type='password')
     if (user_password != password_conf):
         st.warning('Password Mismatch')
 
@@ -174,4 +191,3 @@ if add_selectbox=='SignUp':
     if st.button('Register') and user_password == password_conf:
         Insert_Entry_in_Reg(email, name, user_password)
         st.success('Registration success')
-
